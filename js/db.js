d3d4ecae728b04b5dabc99fed5ba98963b9b5a2b
yuuhi527000-dotcom <yuuhi527000@gmail.com>
2026-05-21 21:10:54 +0900

    Update db.js

--- js/db.js
+++ js/db.js
@@ -1,7 +1,2 @@
 // 語録データはSupabaseで管理しています
-// DBは使用しません
 const DB = {};
-const GENRE = { romance:'恋愛・青春', fantasy:'異世界・ファンタジー', horror:'ホラー・ミステリ', historical:'歴史・時代', general:'汎用' };
-const GENREC = { romance:'gr', fantasy:'gf', horror:'gh', historical:'ghi', general:'gn' };
-const TONE = { poetic:'詩的', classical:'古典', modern:'現代', sensory:'感覚的' };
-const TONEC = { poetic:'tp', classical:'tc', modern:'tm', sensory:'ts' };
